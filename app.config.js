77d3738fbf0860d2e5b54e8662a364e0d93f9e54
hyunsung kim <hyunsungkim@hyunsungui-MacBookPro.local>
2022-06-15 12:46:09 +0900

    app config update

--- app.config.js
+++ app.config.js
@@ -20,7 +20,7 @@ export default {
       supportsTablet: true,
     },
     android: {
-      versionCode: 1,
+      versionCode: 2,
       adaptiveIcon: {
         foregroundImage: './assets/adaptive-icon.png',
         backgroundColor: '#FFFFFF',
@@ -30,15 +30,6 @@ export default {
     },
     web: {
       favicon: './assets/favicon.png',
-    },
-    extra: {
-      firebaseApiKey: process.env.FIREBASE_API_KEY,
-      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
-      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      firebaseAppId: process.env.FIREBASE_APP_ID,
-      firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID,
-    },
+    }
   },
 };
